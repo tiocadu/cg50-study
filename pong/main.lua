17d0b50f01ce72843005f3746afc11577a68f226
Carlos Eduardo <edugaspar@hotmail.com>
2020-05-28 23:03:28 -0300

    Adds new font. Draws paddles and ball

--- pong/main.lua
+++ pong/main.lua
@@ -14,6 +14,9 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    smallFont = love.graphics.newFont('assets/font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -31,14 +34,16 @@ end
 
 function love.draw()
     push:apply('start')
+    
+    -- setting bakcground color to gray
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    love.graphics.printf(
-        'Hello World',
-        0,
-        VIRTUAL_HEIGHT / 2 -6,
-        VIRTUAL_WIDTH,
-        'center'
-    )
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH -10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
 
     push:apply('end')
 end
