1a57b8a9d15567cc6425dfcc6cfbef8bd4c89d19
Carlos Eduardo <edugaspar@hotmail.com>
2020-05-29 22:06:51 -0300

    Adds paddle moviments

--- pong/main.lua
+++ pong/main.lua
@@ -12,9 +12,13 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
     smallFont = love.graphics.newFont('assets/font.ttf', 8)
+    scoreFont = love.graphics.newFont('assets/font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -23,6 +27,14 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    -- initial score values
+    player1Score = 0
+    player2Score = 0
+
+    -- paddle positions on Y axis
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
 -- input controll
@@ -32,17 +44,35 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+
+end
+
 function love.draw()
     push:apply('start')
-    
+
     -- setting bakcground color to gray
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH -10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- paddles
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH -10, player2Y, 5, 20)
 
+    -- ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
 
     push:apply('end')
