102ec7b08cbdb1b3fa89dd9be25ab891dc3f3d5b
Carlos Eduardo <edugaspar@hotmail.com>
2020-05-31 13:19:57 -0300

    debug for ball x position

--- pong/main.lua
+++ pong/main.lua
@@ -190,5 +190,5 @@ function displayFPS()
     love.graphics.setFont(smallFont)
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
-    love.graphics.print('help: ' .. tostring(GAME_STATE), 10, 20)
+    love.graphics.print('ball x: ' .. tostring(math.floor(ball.x)), 10, 20)
 end
