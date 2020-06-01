173e884e456e37478fa3dcb79f1369b54add8098
Carlos Eduardo <edugaspar@hotmail.com>
2020-05-31 22:37:04 -0300

    Adds gravity to bird

--- flappy-bird/Bird.lua
+++ flappy-bird/Bird.lua
@@ -1,5 +1,7 @@
 Bird = Class{}
 
+local GRAVITY = 20
+
 function Bird:init(x, y, width, height)
     self.image = love.graphics.newImage('images/bird.png')
     self.width = self.image:getWidth()
@@ -7,6 +9,21 @@ function Bird:init(x, y, width, height)
 
     self.x = (VIRTUAL_WIDTH / 2) - (self.width / 2)
     self.y = (VIRTUAL_HEIGHT /2) - (self.height / 2)
+
+    self.dy = 4
+end
+
+function Bird:reset()
+    self.x = (VIRTUAL_WIDTH / 2) - (self.width / 2)
+    self.y = (VIRTUAL_HEIGHT /2) - (self.height / 2)
+
+    self.dy = 4
+end
+
+function Bird:update(dt)
+    -- implements gravity
+    self.dy = self.dy + GRAVITY * dt
+    self.y = self.y + self.dy * dt
 end
 
 
