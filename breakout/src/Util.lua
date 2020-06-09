228c942fa5ba22ce2c0476f25771c1bfbd90b35c
Cadu Rocha <carlos.rocha@loft.com.br>
2020-06-09 15:02:08 -0300

    Adds Utils that imports sprite sheets and quads

---
+++ breakout/src/Util.lua
@@ -0,0 +1,63 @@
+-- generation of quads from sprite sheets
+
+function GenerateQuads(atlas, tileWidth, tileHeight)
+  local sheetWidth = atlas:getWidth() / tileWidth
+  local sheetHeight = atlas:getHeight() / tileHeight
+
+  local sheetCounter = 1
+  local spriteSheet = {}
+
+  for x = 0, sheetWidth - 1 do
+    for y = 0, sheetHeight - 1 do
+      spriteSheet[sheetCounter] = love.graphics.newQuad(
+        x * sheetWidth,
+        y * sheetHeight,
+        tileWidth,
+        tileHeight,
+        atlas:getDimensions()
+      )
+      sheetCounter = sheetCounter + 1
+    end
+  end
+
+  return spriteSheet
+end
+
+function table.slice(table, first, last, step)
+  local sliced = {}
+
+  for i = first or 1, last or #table, step or 1 do
+    sliced[#sliced + 1] = table[i]
+  end
+
+  return sliced
+end
+
+function GenerateQuadsPaddles(atlas)
+  local x = 0
+  local y = 64
+
+  local counter = 1
+  local quads = {}
+
+  for i = 0, 3 do
+    -- small
+    quads[counter] = love.graphics.newQuad(x, y, 32, 16, atlas:getDimensions())
+    counter = counter + 1
+    -- medium
+    quads[counter] = love.graphics.newQuad(x + 32, y, 64, 16, atlas:getDimensions())
+    counter = counter + 1
+    -- large
+    quads[counter] = love.graphics.newQuad(x + 96, y, 96, 16, atlas:getDimensions())
+    counter = counter + 1
+    -- huge
+    quads[counter] = love.graphics.newQuad(x, y + 16, 128, 16, atlas:getDimensions())
+    counter = counter + 1
+
+    -- prepare for the next paddle
+    x = 0
+    y = y + 32
+  end
+
+  return quads
+end
