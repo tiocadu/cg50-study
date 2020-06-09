e15a70e9a62cc242236b0797520419ee701d8b7f
Cadu Rocha <carlos.rocha@loft.com.br>
2020-06-09 18:38:09 -0300

    Adds ball collision with bricks and better collision wiht paddle

--- breakout/src/states/PlayState.lua
+++ breakout/src/states/PlayState.lua
@@ -27,13 +27,40 @@ function PlayState:update(dt)
   self.ball:update(dt)
 
   if self.ball:collides(self.paddle) then
+    self.ball.y = self.paddle.y - self.ball.height
     self.ball.dy = - self.ball.dy
+
+    -- implements bounce depending where the ball hits the paddle
+    if self.ball.x < self.paddle.x + self.paddle.width/2 and self.paddle.dx < 0 then
+      self.ball.dx = -50 - 4 * (self.paddle.x + self.paddle.width/2 - self.ball.x)
+    elseif self.ball.x > self.paddle.x + self.paddle.width/2 and self.paddle.dx > 0 then
+      self.ball.dx = 50 + 4 * (self.ball.x - self.paddle.x + self.paddle.width/2)
+    end
+
+
     gSounds['paddle-hit']:play()
   end
 
   for k, brick in pairs(self.bricks) do
     if brick.inPlay and self.ball:collides(brick) then
       brick:hit()
+
+      -- ball position after collision with bricks
+      if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+        self.ball.dx = - self.ball.dx
+        self.ball.x = brick.x - self.ball.width
+      elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
+        self.ball.dx = - self.ball.dx
+        self.ball.x = brick.x + brick.width
+      elseif self.ball.y < brick.y then
+        self.ball.dy = - self.ball.dy
+        self.ball.y = brick.y - self.ball.height
+      else
+        self.ball.dy = - self.ball.dy
+        self.ball.y = brick.y + brick.height
+      end
+
+      self.ball.dy = self.ball.dy * 1.02
     end
   end
 
