ad69bd8d1de1b2d0729103dbfd36a11380618053
Cadu Rocha <carlos.rocha@loft.com.br>
2020-06-09 23:29:28 -0300

    Add new logic to bricks level creation

--- breakout/src/Brick.lua
+++ breakout/src/Brick.lua
@@ -2,12 +2,12 @@ Brick = Class{}
 
 -- we have 5 colors and 4 tiers of bricks for each color
 -- color == (1, 5) and tier == (1, 4)
-function Brick:init(x, y)
+function Brick:init(x, y, color, tier)
   self.width = 32
   self.height = 16
 
-  self.color = math.random(1, 5)
-  self.tier = math.random(1, 4)
+  self.color = color or 1
+  self.tier = tier or 1
 
   self.x = x
   self.y = y
--- breakout/src/LevelMaker.lua
+++ breakout/src/LevelMaker.lua
@@ -6,13 +6,25 @@ function LevelMaker:createMap(level)
   local numRows = math.random(1, 5)
   local numCols = math.random(7, 13)
 
+  local shouldSkipRows = math.random(2) == 1 and true or false
+  local shouldSkipBlocks = math.random(2) == 1 and true or false
+
   for y = 1, numRows do
-    for x = 1, numCols do
-      b = Brick(
-        (x - 1) * 32 + (VIRTUAL_WIDTH - (numCols * 32))/2,
-        y * 16
-      )
-      table.insert(bricks, b)
+    local colColor = math.random(1, 5)
+    local tierColor = math.random(1, 4)
+
+    if  not (shouldSkipRows and y % 2 == 0) then
+      for x = 1, numCols do
+        if  not (shouldSkipBlocks and x % 2 == 0) then
+          b = Brick(
+            (x - 1) * 32 + (VIRTUAL_WIDTH - (numCols * 32))/2,
+            y * 16,
+            colColor,
+            tierColor
+          )
+          table.insert(bricks, b)
+        end
+      end
     end
   end
 
